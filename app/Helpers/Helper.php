5a32fa44740b8e77027c54f0d0ed0ffd2ad99f22
LuckyBoy <iamirfanwebdeveloper@gmail.com>
2019-08-12 16:03:29 +0530

    category display leavel wise

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Vanguard\Helpers;
+
+use Vanguard\Models\Categories;
+use Vanguard\Models\UserQuestionAnwser;
+use Vanguard\Models\Questions;
+
+class Helper
+{
+	public static function examLevel($categoryId, $i = '') {
+		
+		$questions = Questions::where('level', $categoryId)->count();
+		$userQuestionAnwser = UserQuestionAnwser::where('category_id', $categoryId)->count();
+		
+		
+		if ($userQuestionAnwser <= 0 && $i == 0) {
+			return '2';
+		}
+		
+		//dump($questions);
+		//dump($userQuestionAnwser);exit;
+		
+		if ($questions != $userQuestionAnwser && $userQuestionAnwser !='0') {
+			return 3;	
+		} elseif($questions == $userQuestionAnwser) {
+			return 4;
+		} elseif(($questions - $userQuestionAnwser) == $questions && $userQuestionAnwser =='0') {
+			return 2;	
+		}elseif ($userQuestionAnwser =='0') {
+			return 5;
+		}
+	}	
+}
