519896dd8ddffcab2fb373441163d4c4c6c37f61
Irfan Khan <juned.khan@wblsolutions.com>
2019-08-17 15:22:51 +0530

    pdf certificate

--- resources/views/exam/certification.blade.php
+++ resources/views/exam/certification.blade.php
@@ -11,42 +11,53 @@
 @stop
 
 @section('content')
-
 <div class="row">
-    <div class="col-lg-5 col-xl-4">
+		<div class="col-md-12">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">
-                    @lang('app.details')
-                </h5>
-                
+                <h3 class="card-title">Results</h3>
+               <div class="table-responsive" id="users-table-wrapper">
+            <table class="table table-borderless table-striped">
+                <thead>
+                <tr>
+                    <th class="min-width-80">Student</th>
+                    <th class="min-width-150">User Id</th>
+                    <th class="min-width-100">Score</th>
+                    <th class="min-width-80">Percentage Score</th>
+                    <th class="min-width-80">Performance Level</th>
+                    <th class="min-width-150">Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                	<tr>
+	                	<td> K seloma</td>
+	                	<td>222333</td>
+	                	<td>150</td>
+	                	<td>60%</td>
+	                	<td>C</td>
+	                	<td>Completed</td>
+	                </tr>
+                </tbody>
+            </table>
+        </div>
             </div>
         </div>
     </div>
-
-    <div class="col-lg-7 col-xl-8">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">
-                    @lang('app.latest_activity')
-
-                   
-                </h5>
-
-                    <table class="table table-borderless table-striped">
-                        <thead>
-                        <tr>
-                            <th>@lang('app.action')</th>
-                            <th>@lang('app.date')</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                       
-                        </tbody>
-                    </table>
-                
-            </div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+        	<div class="card">
+        		 <div class="card-body">
+        		 	<h4 class="alert alert-success">Thank you for participating on the plas program. You have successfully completed.</h4>
+        		 	<div class="certificate-pdf col-md-5">
+        		 		<h5>Download your certificate below</h5>
+        		 		<object data="{{url('upload/users/certificate/student.pdf')}}" type="application/pdf" width="100%" height="100%" style="height: 250px">
+        		 			<p>Alternative text - include a link <a href="{{url('upload/users/certificate/student.pdf')}}">to the PDF!</a></p>
+						</object><br/>
+						<h5><a href="{{url('upload/users/certificate/student.pdf')}}">Download Certificate</a></h5>
+        		 	</div>
+        		 </div>
+        	</div>
         </div>
     </div>
-</div>
 @stop
